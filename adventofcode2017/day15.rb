092d197ca36481d419f0b8bfd5d04fcd278cfba2
Nikos Filippakis <aesmade@gmail.com>
2017-12-16 21:31:10 +0200

    day15 part 2
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- adventofcode2017/day15.rb
+++ adventofcode2017/day15.rb
@@ -1,8 +1,14 @@
 cura = 591
 curb = 393
 
-puts 40000000.times.select{
-    cura = (cura*16807) % 2147483647
-    curb = (curb*48271) % 2147483647
+puts 5000000.times.select{
+    loop do
+        cura = (cura*16807) % 2147483647
+        break if cura & 0b11 == 0
+    end
+    loop do
+        curb = (curb*48271) % 2147483647
+        break if curb & 0b111 == 0
+    end
     cura & ((1<<16) - 1) == curb & ((1<<16) - 1)
 }.length
