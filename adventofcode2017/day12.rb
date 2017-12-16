984f8cf3b2fb89dcf94842f206fbd7fa46d3ff1c
Nikos Filippakis <aesmade@gmail.com>
2017-12-16 16:29:59 +0200

    day12 part 2
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- adventofcode2017/day12.rb
+++ adventofcode2017/day12.rb
@@ -7,14 +7,19 @@ while l = gets
     h[a.strip.to_i] = b.split(',').map(&:strip).map(&:to_i)
 end
 
-s = [0]
+s = []
 vis = Set.new
+grps = 0
+while vis.length < h.keys.length do
+    grps+=1
+    s.push (h.keys - vis.to_a)[0]
 
-while a = s.pop
-    unless vis.include? a then
-        vis << a
-        s.push *h[a]
+    while a = s.pop
+        unless vis.include? a then
+            vis << a
+            s.push *h[a]
+        end
     end
 end
 
-puts vis.length
+puts grps
