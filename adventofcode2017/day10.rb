744424b9b1d38f2fb842fe41c294650bde9b0698
Nikos Filippakis <aesmade@gmail.com>
2017-12-16 14:17:59 +0200

    day10 part 2
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- adventofcode2017/day10.rb
+++ adventofcode2017/day10.rb
@@ -11,9 +11,14 @@ end
 lst = (0...256).to_a
 skip = 0
 pos = 0
-gets.split(',').map(&:to_i).each{|x|
-    lst = revs(lst, pos, x)
-    pos = (pos + x + skip) % lst.length
-    skip += 1
+inp = gets.strip.chars.map(&:ord) + [17, 31, 73, 47, 23]
+64.times {
+    inp.each{|x|
+        lst = revs(lst, pos, x)
+        pos = (pos + x + skip) % lst.length
+        skip += 1
+    }
 }
-puts lst[0]*lst[1]
+puts (0...16).map {|x|
+    lst[x*16...(x+1)*16].reduce(&:^).to_s(16).rjust(2,'0')
+}.join
