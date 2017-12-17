83940238fc33152f2bc39b073ce1a21445cda0cf
Nikos Filippakis <aesmade@gmail.com>
2017-12-17 15:48:08 +0200

    day16 part 2
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- adventofcode2017/day16.rb
+++ adventofcode2017/day16.rb
@@ -3,17 +3,31 @@ def swp(r, i, j)
     r
 end
 
-puts gets.strip.split(',').reduce(('a'..'p').to_a.join){|r, x|
-    a, b = x[1..-1].split('/')
-    case x[0]
-    when 's'
-        a = a.to_i
-        r[-a..-1] + r[0...-a]
-    when 'x'
-        swp(r, a.to_i, b.to_i)
-    when 'p'
-        swp(r, r.index(a), r.index(b))
+inp = ('a'..'p').to_a.join 
+steps = gets.strip.split(',')
+s = {}
+cur = 0
+while cur < 1000000000
+    if s.include? inp
+        dif = cur-s[inp]
+        cur += dif while cur + dif < 1000000000
     else
-        'wtfwtfwtfwtfwtfwtfwtfwtfwtfwtfwtfwtf'
+        s[inp] = cur
     end
-}
+    inp = steps.reduce(inp){|r, x|
+        a, b = x[1..-1].split('/')
+        case x[0]
+        when 's'
+            a = a.to_i
+            r[-a..-1] + r[0...-a]
+        when 'x'
+            swp(r, a.to_i, b.to_i)
+        when 'p'
+            swp(r, r.index(a), r.index(b))
+        else
+            'wtfwtfwtfwtfwtfwtfwtfwtfwtfwtfwtfwtf'
+        end
+    }
+    cur += 1
+end
+puts inp
