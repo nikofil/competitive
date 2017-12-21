2896365262b6bfaf3492df9e85b636679da04e3f
Nikos Filippakis <aesmade@gmail.com>
2017-12-21 16:56:50 +0200

    day21 part 1
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- adventofcode2017/day21.rb
+++ adventofcode2017/day21.rb
@@ -2,12 +2,11 @@ h = STDIN.read.split("\n").map{|x|
     x.split(" => ")
 }.to_h
 
-puts "#{h}"
 
 def s2boxes(s)
     ss = s.split('/')
     if ss.length == 4
-        [[ss[0][0..1], ss[1][0..1]], [ss[2][0..1], ss[3][0..1]], [ss[0][2..3], ss[1][2..3]], [ss[2][2..3], ss[3][2..3]]].map{|x| x.join('/')}
+        fuckingjoin([[ss[0][0..1], ss[1][0..1]], [ss[0][2..3], ss[1][2..3]], [ss[2][0..1], ss[3][0..1]], [ss[2][2..3], ss[3][2..3]]].map{|x| x.join('/')})
     else
         s
     end
@@ -27,18 +26,42 @@ def rot(s)
     }.join '/'
 end
 
-boxes = ['.#./..#/###']
+def fuckingjoin(x)
+    side = x[0].split('/')[0].length
+    sidesmol = Math.sqrt(x.length).to_i
+    side2 = sidesmol * side
+    res = (0...side2).map{|i|
+        (0...side2).map{|j|
+            x[(i/side)*sidesmol + j/side].split('/')[i%side][j%side]
+        }.join
+    }.join '/'
+    res
+end
+
+def fuckingsplit(x)
+    y = x.split '/'
+    sp = if y.length % 2 == 0; 2 else 3 end
+    res = 0.step(y.length-1, sp).map{|k|
+        0.step(y.length-1, sp).map{|l|
+            (0...sp).map{|i|
+                (0...sp).map{|j| y[k+i][l+j]}.join
+            }.join '/'
+        }
+    }.flatten
+    res
+end
+
+boxes = '.#./..#/###'
 
 5.times{
-    boxes.map!{|x|
+    boxes = fuckingjoin(fuckingsplit(boxes).map{|x|
         n = 3.times.reduce([x]){|acc|
             acc + [rot(acc[-1])]
         }.map{|x| [x, flip(x)]}.flatten
         n.map!{|x| h[x]}.select!(&:itself)
         puts "wtf" if n[1..-1].any?{|x| x != n[0]}
         s2boxes(n[0])
-    }.flatten!
-    #puts "#{boxes}"
+    })
 }
 
-puts "#{boxes.join.count '#'}"
+puts "#{boxes.count '#'}"
