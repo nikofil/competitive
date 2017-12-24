96ebd9bdcd6735de295190461548b85e2a470043
Nikos Filippakis <aesmade@gmail.com>
2017-12-24 12:02:36 +0200

    day24 part 2
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- adventofcode2017/day24.rb
+++ adventofcode2017/day24.rb
@@ -9,13 +9,13 @@ $h = STDIN.read.split("\n").each_with_index.map{|x, idx|
     [x, Set.new(y.map{|z| z[1]})]
 }.to_h
 
-def best(cur, vis)
+def best(cur, vis, depth)
     meb = $h[cur] - vis
     return 0 if meb.empty?
     meb.map{|x|
         nxt = if $val[x][0] == cur then $val[x][1] else $val[x][0] end
-        best(nxt, vis + [x]) + $val[x].sum
+        (best(nxt, vis + [x], depth+1) + $val[x].sum) + depth*1000000000
     }.max
 end
 
-puts best(0, Set.new)
+puts best(0, Set.new, 0) % 1000000000
